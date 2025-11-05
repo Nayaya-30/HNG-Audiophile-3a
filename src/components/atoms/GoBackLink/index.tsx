aa26fc715d30dabd20cb9b7c082437e6c7b8361f
Usouff <nayaya95.30@gmail.com>
2025-11-05 08:25:07 +0100

    feat(atoms): CategoryLink, FormField, Radio, GoBackLink with <picture> breakpoints

---
+++ src/components/atoms/GoBackLink/index.tsx
@@ -0,0 +1,20 @@
+import { Button } from '@chakra-ui/react'
+import { useRouter } from 'next/router'
+
+const GoBackLink = (): JSX.Element => {
+  const router = useRouter()
+
+  return (
+    <Button
+      as="a"
+      variant="link"
+      onClick={() => router.back()}
+      mt={{ base: '2rem', sm: '1rem', lg: '5rem' }}
+      fontSize="0.9375rem"
+    >
+      Go back
+    </Button>
+  )
+}
+
+export default GoBackLink
