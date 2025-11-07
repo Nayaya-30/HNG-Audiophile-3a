a76152264bd620f444da1ec399bca9fe184d3bbb
Usouff <nayaya95.30@gmail.com>
2025-11-07 12:54:27 +0100

    import fix

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,4 +1,4 @@
-i nodemailerimport React from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { api } from "../_generated/api";
 import { useMutation } from "convex/react";
