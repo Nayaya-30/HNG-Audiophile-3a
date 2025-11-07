6e4b8fb57e7e34c2e1ec985a221b35ee07df5081
Usouff <nayaya95.30@gmail.com>
2025-11-07 04:29:27 +0100

    installed missing dependency

---
+++ src/test_extend.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// test_extend.ts
+var react_1 = require("@chakra-ui/react");
+console.log(typeof react_1.extendTheme);
---
+++ src/test_extend.ts
@@ -0,0 +1,4 @@
+// test_extend.ts
+import { extendTheme } from "@chakra-ui/react";
+
+console.log(typeof extendTheme);
